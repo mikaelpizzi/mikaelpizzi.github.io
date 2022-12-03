fe703006287cadcd598bc3b9ea5d2cc72c62ecb7
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-03 19:52:04 -0400

    feat: Make text responsive

--- components/text.js
+++ components/text.js
@@ -4,13 +4,13 @@ import { mainText } from "../constants"
 const Text = () => {
     return ( 
         <div>
-            <h1 className="text-[#ff0000] text-center font-extrabold text-[48px] font-['Source_Serif_Pro']">Hi, I'm Stupid</h1>
+            <h1 className="text-[#ff0000] text-center font-extrabold sm:text-[48px] text-[32px] font-['Source_Serif_Pro']">Hi, I'm Stupid</h1>
 
-            <div className="text-left py-8 text-lg">
-            <p className="my-5">{mainText[0]}</p>
-            <p className="my-5">{mainText[1]}</p>
-            <p className="my-5">{mainText[2]}</p>
-            <p className="my-5">i'm thinking what tf write lol</p>
+            <div className="text-left py-8 sm:text-lg text-[1.05rem]">
+                <p className="mb-5">{mainText[0]}</p>
+                <p className="my-5">{mainText[1]}</p>
+                <p className="my-5">{mainText[2]}</p>
+                <p className="my-5">i'm thinking what tf write lol</p>
             </div>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <div className="text-center lg:px-[10%] xl:px-[16%] px-0 lg:bg-[#1e1b01] bg-white width-full">
-      <div className= "bg-slate-100 lg:px-24 px-8 py-8 width-full">
+      <div className= "bg-slate-100 lg:px-24 sm:px-8 px-6 py-8 width-full">
 
           <Header />
 
