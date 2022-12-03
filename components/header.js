60b80591f8c22af22672cd3a82bbbd8525f6a64b
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-03 18:59:57 -0400

    feat: Add header component

---
+++ components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <header>
+            <img className="portrait" src="/logo.jpg" alt='portrait photo' />
+        </header>
+    );
+}
+ 
+export default Header;
