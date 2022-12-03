109d171187d27989803bad4dd8272c398e802192
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-03 19:01:01 -0400

    add: Add constants folder for texts

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const mainText = [
+    "If I was stupid a week ago, imagine how stupid I was when I thought HTML was a programming language.",
+    "I'm sure I'm stupid today, but tomorrow I'll be less so.",
+    "Now I am a developer who learns every day, trying to change the world for the better, my world, your world."
+];
