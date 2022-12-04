f88501308f611e2c520ff34231e7305ed6b71bea
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-03 22:55:59 -0400

    feat: Add styles and icons to header

--- components/header.js
+++ components/header.js
@@ -1,11 +1,44 @@
+import { motion } from "framer-motion";
+
 const Header = () => {
     return ( 
-        <header>
-            <img 
-                src="/logo.jpg" 
-                alt='portrait photo'
-                className="mb-4 pb-4" 
+        <header className="flex items-center justify-around mb-4 pb-4">
+            <a href="!#" target="_blank">
+                <motion.img 
+                    whileHover={{ scale: 1.15 }}
+                    src="/logo.jpg" 
+                    alt='portrait photo'
+                />
+            </a>
+            
+            <div className="flex">
+            <a href="!#" target="_blank">
+            <motion.img
+                whileHover={{ scale: 1.15 }}
+                src="/email.png"
+                alt="email"
+                className="sm:w-[40px] w-[25px] sm:mr-16 mr-4 ml-4"
             />
+            </a>
+
+            <a href="!#" target="_blank">
+            <motion.img
+                whileHover={{ scale: 1.15 }}
+                src="/linkedin.png"
+                alt="email"
+                className="sm:w-[40px] w-[25px] sm:mr-16 mr-4"
+            />
+            </a>
+
+            <a href="!#" target="_blank">
+            <motion.img
+                whileHover={{ scale: 1.15 }}
+                src="/twitter.png"
+                alt="email"
+                className="sm:w-[40px] w-[25px]"
+            />
+            </a>
+            </div>
         </header>
     );
 }
