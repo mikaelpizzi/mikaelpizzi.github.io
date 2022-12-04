4e8561c091bc8001bcc34ddfc7a8635804a56e0a
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-03 23:12:31 -0400

    fix: Fix anchor links

--- components/header.js
+++ components/header.js
@@ -12,32 +12,32 @@ const Header = () => {
             </a>
             
             <div className="flex">
-            <a href="!#" target="_blank">
-            <motion.img
-                whileHover={{ scale: 1.15 }}
-                src="/email.png"
-                alt="email"
-                className="sm:w-[40px] w-[25px] sm:mr-16 mr-4 ml-4"
-            />
-            </a>
+                <a href="mailto:mikaelangelopizzi@gmail.com" target="_blank">
+                    <motion.img
+                        whileHover={{ scale: 1.15 }}
+                        src="/email.png"
+                        alt="email"
+                        className="sm:w-[40px] w-[25px] sm:mr-16 mr-4 ml-4"
+                    />
+                </a>
 
-            <a href="!#" target="_blank">
-            <motion.img
-                whileHover={{ scale: 1.15 }}
-                src="/linkedin.png"
-                alt="email"
-                className="sm:w-[40px] w-[25px] sm:mr-16 mr-4"
-            />
-            </a>
+                <a href="https://www.linkedin.com/in/mikaelpizzi/" target="_blank">
+                    <motion.img
+                        whileHover={{ scale: 1.15 }}
+                        src="/linkedin.png"
+                        alt="email"
+                        className="sm:w-[40px] w-[25px] sm:mr-16 mr-4"
+                    />
+                </a>
 
-            <a href="!#" target="_blank">
-            <motion.img
-                whileHover={{ scale: 1.15 }}
-                src="/twitter.png"
-                alt="email"
-                className="sm:w-[40px] w-[25px]"
-            />
-            </a>
+                <a href="https://twitter.com/mikaelpizzi" target="_blank">
+                    <motion.img
+                        whileHover={{ scale: 1.15 }}
+                        src="/twitter.png"
+                        alt="email"
+                        className="sm:w-[40px] w-[25px]"
+                    />
+                </a>
             </div>
         </header>
     );
