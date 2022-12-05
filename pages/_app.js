ccd0e85447f941c8d49d0ff537a3b63a8be10d60
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-04 21:23:03 -0400

    fix: Fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mikael Pizzi</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
