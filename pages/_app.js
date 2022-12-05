7af91f2a46e4ef3c9515288f11e8cf0b96a9116d
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-12-04 21:49:02 -0400

    imp: Improve title

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Mikael Pizzi</title>
+        <title>Surely you have felt like this more than once</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
