7ae5e1a2a3217d0f7ff72ada91d37de68d5cfa05
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-12 23:35:37 -0400

    feat: Add "Instagram 2" project

--- components/projects.js
+++ components/projects.js
@@ -18,6 +18,61 @@ const Projects = () => {
               className="md:w-[60%] w-auto"
             >
               <a
+                href='https://instagram2-mikaelpizzi.vercel.app/'
+                target='_blank'
+              >
+                <motion.img
+                  whileHover={{ scale: 1.075 }}
+                  whileTap={{ scale: 0.8 }}
+                  src='/instagram2.jpg'
+                  alt='Metaversus project image'
+                  className="w-full rounded-lg shadow-[0_0px_20px_2px_#000] hover:shadow-none hover:border-[1px] border-black md:mb-0 mb-6"
+                  />
+              </a>
+            </motion.div>
+            <motion.div 
+              variants={fadeIn('right', 'tween', 0.2, 0.5)}
+              className="text-[18px] justify-start md:ml-8 flex-grow"
+            >
+              <h3 className="font-bold pb-4 min-[832px]:text-[20px] lg:text-[24px]">Instagram 2</h3>
+              <ul className="md:text-[16px] min-[832px]:text-[18px] text-center">
+                <li className="font-medium mb-4">Instagram clone made with NextJS, NextAuth, Firebase v9, Recoil, FakerJS and TailwindCSS</li>
+                <li>Custom NextAuth</li>
+                <li>Firebase v9</li>
+                <li>FakerJS</li>
+              </ul>
+              <div className="flex md:justify-around justify-evenly mt-8">
+                <motion.a
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                  href='https://instagram2-mikaelpizzi.vercel.app/'
+                  target='_blank'
+                  className='bg-black text-white font-bold py-2 px-4 rounded'
+                >LIVE</motion.a>
+
+                <motion.a
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                  href='https://github.com/mikaelpizzi/instagram2'
+                  target='_blank'
+                  className='bg-black text-white font-bold py-2 px-4 rounded'
+                >CODE</motion.a>
+              </div>
+            </motion.div>
+          </motion.div>
+
+          <motion.div 
+            variants={staggerContainer}
+            initial='hidden'
+            whileInView='show'
+            viewport={{ once: false, amount: 0.25}}
+            className="flex md:flex-row flex-col mt-[80px] mb-[120px]"
+          >
+            <motion.div 
+              variants={fadeIn('right', 'tween', 0.2, 0.5)}
+              className="md:w-[60%] w-auto"
+            >
+              <a
                 href='https://metaversus-mikaelpizzi.vercel.app/'
                 target='_blank'
               >
