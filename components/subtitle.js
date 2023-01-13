427858324c35a7aa9b40982681a79bff0481cf52
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-12 23:55:01 -0400

    imp: Improve subtitle styles

--- components/subtitle.js
+++ components/subtitle.js
@@ -1,7 +1,7 @@
 const Subtitle = () => {
   return (
     <h3 
-        className="font-bold pt-20 min-[832px]:text-[20px] lg:text-[24px]"
+        className="font-bold pt-20 text-[20px] min-[832px]:text-[24px] lg:text-[28px]"
     >
         Full stack Javascript
     </h3>
