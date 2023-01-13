42213fadeea3a3daad733fc771abd0b92fb8764e
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-12 23:48:25 -0400

    del: Delete unnecessary project

--- components/projects.js
+++ components/projects.js
@@ -61,6 +61,7 @@ const Projects = () => {
             </motion.div>
           </motion.div>
 
+
           <motion.div 
             variants={staggerContainer}
             initial='hidden'
@@ -115,6 +116,7 @@ const Projects = () => {
               </div>
             </motion.div>
           </motion.div>
+
 
           <motion.div 
             variants={staggerContainer}
@@ -226,63 +228,6 @@ const Projects = () => {
               </div>
             </motion.div>
           </motion.div>
-
-
-          <motion.div 
-            variants={staggerContainer}
-            initial='hidden'
-            whileInView='show'
-            viewport={{ once: false, amount: 0.25}}
-            className="flex md:flex-row flex-col mt-[120px] mb-[60px]"
-          >
-            <motion.div 
-              variants={fadeIn('right', 'tween', 0.2, 0.5)}
-              className="md:w-[60%] w-auto"
-            >
-              <a
-                href='https://mikaelpizzi-crud-redux.netlify.app/'
-                target='_blank'
-              >
-                <motion.img
-                  whileHover={{ scale: 1.075 }}
-                  whileTap={{ scale: 0.8 }}
-                  src='/crud-redux.jpg'
-                  alt='CRUD Redux project image'
-                  className="width-full rounded-lg shadow-[0_0px_20px_2px_#000] hover:shadow-none hover:border-[1px] border-black md:mb-0 mb-6"
-                  />
-              </a>
-            </motion.div>
-            <motion.div 
-              variants={fadeIn('right', 'tween', 0.2, 0.5)}
-              className="text-[18px] justify-start md:ml-8 flex-grow"
-            >
-              <h3 className="font-bold pb-4 min-[832px]:text-[20px] lg:text-[24px]">CRUD-Redux</h3>
-              <ul className="md:text-[16px] min-[832px]:text-[18px]">
-                <li className="font-medium mb-4">A CRUD web application using React, Redux, REST API & Axios</li>
-                <li>Redux</li>
-                <li>Rest API development</li>
-                <li>Bootswatch</li>
-              </ul>
-              <div className="flex md:justify-around justify-evenly mt-8">
-                <motion.a
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.8 }}
-                  href='https://mikaelpizzi-crud-redux.netlify.app/'
-                  target='_blank'
-                  className='bg-black text-white font-bold py-2 px-4 rounded'
-                >LIVE</motion.a>
-
-                <motion.a
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.8 }}
-                  href='https://github.com/mikaelpizzi/crud-redux'
-                  target='_blank'
-                  className='bg-black text-white font-bold py-2 px-4 rounded'
-                >CODE</motion.a>
-              </div>
-            </motion.div>
-          </motion.div>
-
         </section>
     );
 }
