83495ed28188a502e44bd878f2fafc0a51b765f1
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-06-03 17:08:59 -0400

    del: Delete uneccessary code

--- components/subtitle.js
+++ components/subtitle.js
@@ -1,11 +1,9 @@
 const Subtitle = () => {
   return (
-    <h3 
-        className="font-bold pt-20 text-[20px] min-[832px]:text-[24px] lg:text-[28px]"
-    >
-        Full stack Javascript
+    <h3 className="font-bold pt-8 text-[20px] min-[832px]:text-[24px] lg:text-[28px]">
+      MongoDB, Express, React, NodeJS, Firebase and JS related:
     </h3>
-  )
-}
+  );
+};
 
 export default Subtitle;
