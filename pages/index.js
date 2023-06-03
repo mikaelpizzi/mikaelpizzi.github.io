13b8bc48e8c0a095ee77a947bd109f10961d6fcb
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-06-03 17:12:00 -0400

    feat: Display updates

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-import Projects from "../components/projects";
-import Meet from "../components/calendly";
+import JavascriptProjects from "../components/javascriptProjects";
 import Text from "../components/text";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Subtitle from "../components/subtitle";
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div className="text-center lg:px-[10%] xl:px-[16%] px-0 lg:bg-[#1e1b01] bg-white width-full">
-      <div className= "bg-slate-100 lg:px-24 sm:px-8 px-6 py-8 width-full">
-          <Header />
-          <Text />
-          <Meet />
-          <Subtitle />
-          <Projects />
-          <Meet />
-          <Footer />
+      <div className="bg-slate-100 lg:px-24 sm:px-8 px-6 py-8 width-full">
+        <Header />
+        <Text />
+        <Subtitle />
+        <JavascriptProjects />
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
